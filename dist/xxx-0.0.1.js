0fafe3226085a9ec4b97b49c5dd7958460e5a451
Stephen Young <young.steveo@gmail.com>
2013-01-31 23:05:45 -0500

    Running grunt...  All good.

---
+++ dist/xxx-0.0.1.js
@@ -0,0 +1,10 @@
+/**
+ * xxx v0.0.1 - 2013-01-31
+ * 
+ *
+ * Copyright (c) 2013 Stephen Young <young.steveo@gmail.com>
+ * Licensed MIT
+ */
+;(function(undefined){
+    "use strict";
+}).call(this);
---
+++ dist/xxx-0.0.1.min.js
@@ -0,0 +1,1 @@
+(function(){"use strict"}).call(this);
